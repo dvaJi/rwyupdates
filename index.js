a6d0dd83d90a90cf4cdf57dd6296288b00caf8eb
dvaJi <francisco_p@outlook.com>
2020-03-12 00:07:28 -0300

    chore: update puppeter version and latest MU updates

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const puppeteer = require('puppeteer');
-const csvWriter = require('csv-write-stream');
-const argv = require('yargs').argv;
-const fs = require('fs');
+const puppeteer = require("puppeteer");
+const argv = require("yargs").argv;
+const fs = require("fs");
+const franc = require("franc-all");
+const parse = require("date-fns/parse");
 
-const SELECTOR = require('./utils/selectors');
-const actualLog = require('./data/log.json');
+const SELECTOR = require("./utils/selectors");
+const actualLog = require("./data/log.json");
+const dataJson = require("./data/data.json");
 
 const TO_SAVE = parseInt(argv.files, 0) || 1;
 
@@ -15,97 +17,138 @@ async function run() {
 
   const page = await browser.newPage();
 
-  var writer = csvWriter({ sendHeaders: false, separator: ',' });
-  writer.pipe(fs.createWriteStream('./data/data.csv', { flags: 'a' }));
-
-  const LAST_MANGA = actualLog[actualLog.length - 1]
-    ? actualLog[actualLog.length - 1].id
-    : 0;
+  // const LAST_MANGA = actualLog[actualLog.length - 1]
+  //   ? actualLog[actualLog.length - 1].id
+  //   : 0;
+  const LAST_MANGA = 152771;
   const LOG = [...actualLog];
+  const dataArray = [...dataJson];
 
+  console.log(LAST_MANGA, LAST_MANGA + TO_SAVE);
   for (let index = LAST_MANGA + 1; index <= LAST_MANGA + TO_SAVE; index++) {
-    const TIME = 10000 + index;
+    const TIME = 15000 + Math.floor(Math.random() * (100 - 1 + 1) + 1);
     await page.waitFor(TIME);
     console.log(`Fetching manga id:${index}`);
     await page.goto(`https://www.mangaupdates.com/series.html?id=${index}`);
     await page.screenshot({ path: `screenshots/manga_${index}.jpg` });
 
     // Check if is a valid id
-    const check = await page.evaluate(sel => {
+    const isValid = await page.evaluate(sel => {
       const element = document.querySelector(sel);
-      return element ? element.textContent.trim() : null;
+      return element && element.textContent.trim() === "Error"
+        ? element.textContent.trim()
+        : null;
     }, SELECTOR.ERROR);
 
-    if (check !== null) {
+    if (isValid !== null) {
       const errorMsg = await page.evaluate(sel => {
         const element = document.querySelector(sel);
         return element ? element.textContent.trim() : null;
       }, SELECTOR.ERROR_MSG);
-      LOG.push({ id: index, status: 'ERROR', msg: errorMsg });
+      LOG.push({ id: index, status: "ERROR", msg: errorMsg });
       continue;
     }
 
+    // Name
     const name = await page.evaluate(sel => {
       return document.querySelector(sel).innerHTML;
     }, SELECTOR.MAIN_TITLE);
-    //console.log(name);
 
-    const description = await page.evaluate(sel => {
-      return document.querySelector(sel).textContent.trim();
-    }, SELECTOR.DESCRIPTION);
-    //console.log(description);
+    // Description
+    let description = "";
+    try {
+      description = await page.evaluate(sel => {
+        // Remove 'Less' hyperlink
+        var elements = document.querySelectorAll("#div_desc_more > a");
+        for (var i = 0; i < elements.length; i++) {
+          elements[i].parentNode.removeChild(elements[i]);
+        }
+        return document.querySelector(sel).textContent.trim();
+      }, "#div_desc_more");
+    } catch (e) {
+      description = await page.evaluate(sel => {
+        return document.querySelector(sel).textContent.trim();
+      }, SELECTOR.DESCRIPTION);
+    }
 
+    // Type
     const type = await page.evaluate(sel => {
       return document.querySelector(sel).innerHTML.trim();
     }, SELECTOR.TYPE);
-    //console.log(type);
 
     const relatedSeries = await page.evaluate(sel => {
-      let html = document.querySelector(sel).innerText;
-      return html.trim().replace(/\n/gi, ' | ');
-    }, SELECTOR.RELATED_SERIES);
-    //console.log(relatedSeries);
+      if (document.querySelector(sel).textContent === "N/A") {
+        return [];
+      }
 
-    const associatedNames = await page.evaluate(sel => {
-      let html = document.querySelector(sel).innerText;
-      return html.trim().replace(/\n/gi, ' | ');
-    }, SELECTOR.ASSOCIATED_NAMES);
-    //console.log(associatedNames);
+      let html = document.querySelector(sel).innerHTML;
+      const rseries = html.trim().split("<br>");
+      return rseries
+        .map(ser => {
+          try {
+            const RS_HREF_REGEX = /href="(.*?)"/g;
+            const HTML_TAGS_REGEX = /(?:<[^>]+>)*/g;
+            const LAST_PARENTHESIS_REGEX = /\(([^)]*)\)[^(]*$/g;
+            const asdfsd = ser.replace(HTML_TAGS_REGEX, "");
+            return {
+              id: ser
+                .match(RS_HREF_REGEX)[0]
+                .match(/\d+/g)[0]
+                .replace("series.html?id=", ""),
+              name: asdfsd.replace(LAST_PARENTHESIS_REGEX, "").trim(),
+              relation: asdfsd
+                .match(LAST_PARENTHESIS_REGEX)[0]
+                .replace(/(\(|\))+/g, "")
+            };
+          } catch (err) {
+            return err;
+          }
+        })
+        .filter(rs => rs !== {});
+    }, SELECTOR.RELATED_SERIES);
+
+    const associatedNames = (
+      await page.evaluate(sel => {
+        let html = document.querySelector(sel).innerText;
+        return html.trim().split(/\n/gi);
+      }, SELECTOR.ASSOCIATED_NAMES)
+    ).map(name => {
+      const lang = franc(name);
+      return {
+        name: name,
+        lang: lang !== "und" ? lang : undefined
+      };
+    });
 
     const statusInCountryOfOrigin = await page.evaluate(sel => {
       let html = document.querySelector(sel).innerText;
-      return html.trim().replace(/\n/gi, ' | ');
+      return html.trim().split(/\n/gi);
     }, SELECTOR.STATUS_IN_COUNTRY_OF_ORIGIN);
-    //console.log(statusInCountryOfOrigin);
 
     const completelyScanlated = await page.evaluate(sel => {
       return document.querySelector(sel).textContent.trim();
     }, SELECTOR.COMPLETELY_SCANLATED);
-    //console.log(completelyScanlated);
 
     const lastUpdated = await page.evaluate(sel => {
       return document.querySelector(sel).textContent.trim();
     }, SELECTOR.LAST_UPDATED);
-    //console.log(lastUpdated);
 
     const genres = await page.evaluate(sel => {
       let html = Array.from(document.querySelectorAll(sel));
       return html
-        .filter(g => g.textContent !== 'Search for series of same genre(s)')
+        .filter(g => g.textContent !== "Search for series of same genre(s)")
         .map(genre => {
           return genre.textContent;
         });
     }, SELECTOR.GENRES);
-    //console.log(genres);
 
     let categoryLength = await page.evaluate(sel => {
       return document.querySelectorAll(sel).length;
     }, SELECTOR.CATEGORIES_LIST);
-    //console.log({ categoryLength });
 
     let categories = [];
     for (let i = 1; i <= categoryLength + 1; i++) {
-      const CATEGORY_SELECTOR = SELECTOR.CATEGORY.replace('*INDEX*', i);
+      const CATEGORY_SELECTOR = SELECTOR.CATEGORY.replace("*INDEX*", i);
 
       let category = await page.evaluate(sel => {
         const el = document.querySelector(sel);
@@ -116,63 +159,77 @@ async function run() {
         categories.push(category);
       }
     }
-    //console.log(categories);
 
     const authors = await page.evaluate(sel => {
-      let html = document.querySelector(sel).innerText;
-      return html.trim().replace(/\n/gi, ' | ');
+      let html = document.querySelectorAll(sel);
+      let authors = [];
+
+      for (let index = 0; index < html.length; index++) {
+        const element = html[index];
+        authors.push({
+          name: element.innerText.trim(),
+          id: Number(element.getAttribute("href").replace(/\D+/g, ''))
+        });
+      }
+
+      return authors;
     }, SELECTOR.AUTHORS);
     //console.log(authors);
 
     const artists = await page.evaluate(sel => {
-      let html = document.querySelector(sel).innerText;
-      return html.trim().replace(/\n/gi, ' | ');
+      let html = document.querySelectorAll(sel);
+      let artists = [];
+
+      for (let index = 0; index < html.length; index++) {
+        const element = html[index];
+        artists.push({
+          name: element.innerText.trim(),
+          id: Number(element.getAttribute("href").replace(/\D+/g, ''))
+        });
+      }
+
+      return artists;
     }, SELECTOR.ARTISTS);
-    //console.log(artists);
 
     const year = await page.evaluate(sel => {
       return document.querySelector(sel).innerHTML.trim();
     }, SELECTOR.YEAR);
-    //console.log(year);
 
     const originalPublishers = await page.evaluate(sel => {
       let html = document.querySelector(sel).innerText;
-      return html.trim().replace(/\n/gi, ' | ');
+      return html.trim().split(/\n/gi);
     }, SELECTOR.ORIGINAL_PUBLISHER);
-    //console.log(originalPublishers);
 
     const serializedIn = await page.evaluate(sel => {
       let html = document.querySelector(sel).innerText;
-      return html.trim().replace(/\n/gi, ' | ');
+      return html.trim().split(/\n/gi);
     }, SELECTOR.SERIALIZED_IN);
-    //console.log(serializedIn);
 
     const licensed = await page.evaluate(sel => {
       return document.querySelector(sel).innerHTML.trim();
     }, SELECTOR.LICENSED_ENGLISH);
-    //console.log(licensed);
 
     const englishPublisher = await page.evaluate(sel => {
       let html = document.querySelector(sel).innerText;
-      return html.trim().replace(/\n/gi, ' | ');
+      return html.trim().split(/\n/gi);
     }, SELECTOR.ENGLISH_PUBLISHER);
-    //console.log(englishPublisher);
 
     const cover = await page.evaluate(sel => {
       const doc = document.querySelector(sel);
       if (doc) {
-        return doc.getAttribute('src');
+        return doc.getAttribute("src");
       } else {
         return null;
       }
     }, SELECTOR.COVER);
-    //console.log(cover);
 
     const userRating = await page.evaluate(sel => {
       const doc = document.querySelector(sel).innerHTML;
-      return doc.substring(0, doc.indexOf('<br>'));
+      return doc
+        .substring(0, doc.indexOf("<br>"))
+        .replace('<span class="d-none d-sm-inline">', "")
+        .replace("</span>", "");
     }, SELECTOR.USER_RATING);
-    //console.log(userRating);
 
     const serie = {
       id: index,
@@ -196,20 +253,29 @@ async function run() {
       cover,
       userRating
     };
-    //console.log(serie);
+    console.log(serie);
 
-    writer.write(serie);
-    LOG.push({ id: index, status: 'OK', msg: '' });
-    await saveLog(LOG);
+    // dataArray.push(serie);
+    // LOG.push({ id: index, status: "OK", msg: "" });
+    // await saveToJson(dataArray);
+    // await saveLog(LOG);
   }
 
-  writer.end();
   browser.close();
 }
 
 async function saveLog(log) {
   const content = await JSON.stringify(log);
-  fs.writeFile('./data/log.json', content, 'utf8', function(err) {
+  fs.writeFile("./data/log.json", content, "utf8", function(err) {
+    if (err) {
+      return console.error(err);
+    }
+  });
+}
+
+async function saveToJson(data) {
+  const content = await JSON.stringify(data);
+  fs.writeFile("./data/data.json", content, "utf8", function(err) {
     if (err) {
       return console.error(err);
     }
--- utils/selectors.js
+++ utils/selectors.js
@@ -1,42 +1,40 @@
-module.exports.ERROR =
-  '#main_content > table > tbody > tr > td > table > tbody > tr:nth-child(1) > td.tab_middle';
-module.exports.ERROR_MSG =
-  '#main_content > table > tbody > tr > td > table > tbody > tr:nth-child(2) > td';
+module.exports.ERROR = "#main_content > div > div > div:nth-child(1) > div";
+module.exports.ERROR_MSG = "#main_content > div > div > div:nth-child(2) > div";
 module.exports.MAIN_TITLE =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(1) > span.releasestitle.tabletitle';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div.col-12.p-2 > span.releasestitle.tabletitle";
 module.exports.DESCRIPTION =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(3) > div > div:nth-child(2)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(3) > div:nth-child(2)";
 module.exports.TYPE =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(3) > div > div:nth-child(5)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(3) > div:nth-child(5)";
 module.exports.RELATED_SERIES =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(3) > div > div:nth-child(8)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(3) > div:nth-child(8)";
 module.exports.ASSOCIATED_NAMES =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(3) > div > div:nth-child(11)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(3) > div:nth-child(11)";
 module.exports.STATUS_IN_COUNTRY_OF_ORIGIN =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(3) > div > div:nth-child(20)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(3) > div:nth-child(20)";
 module.exports.COMPLETELY_SCANLATED =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(3) > div > div:nth-child(23)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(3) > div:nth-child(23)";
 module.exports.LAST_UPDATED =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(3) > div > div:nth-child(38)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(3) > div:nth-child(38)";
 module.exports.GENRES =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(4) > div > div:nth-child(5) > a';
-module.exports.CATEGORIES_LIST = '#ajax_tag_data > ul > li';
-module.exports.CATEGORY = '#ajax_tag_data > ul > li:nth-child(*INDEX*)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(4) > div:nth-child(5) > a";
+module.exports.CATEGORIES_LIST = "#ajax_tag_data > ul > li";
+module.exports.CATEGORY = "#ajax_tag_data > ul > li:nth-child(*INDEX*)";
 module.exports.AUTHORS =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(4) > div > div:nth-child(17)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(4) > div:nth-child(17) > a";
 module.exports.ARTISTS =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(4) > div > div:nth-child(20)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(4) > div:nth-child(20) > a";
 module.exports.YEAR =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(4) > div > div:nth-child(23)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(4) > div:nth-child(23)";
 module.exports.ORIGINAL_PUBLISHER =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(4) > div > div:nth-child(26)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(4) > div:nth-child(26)";
 module.exports.SERIALIZED_IN =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(4) > div > div:nth-child(29)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(4) > div:nth-child(29)";
 module.exports.LICENSED_ENGLISH =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(4) > div > div:nth-child(32)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(4) > div:nth-child(32)";
 module.exports.ENGLISH_PUBLISHER =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(4) > div > div:nth-child(35)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(4) > div:nth-child(35)";
 module.exports.COVER =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(4) > div > div:nth-child(2) > center > img';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(4) > div:nth-child(2) > center > img";
 module.exports.USER_RATING =
-  '#main_content > table.series_content_table > tbody > tr > td > div:nth-child(1) > div:nth-child(3) > div > div:nth-child(35)';
+  "#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(3) > div:nth-child(35)";
